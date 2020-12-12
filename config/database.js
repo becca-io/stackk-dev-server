125c821a5203819a2c666183f395561a13681e91
JaeYeopHan <ljyhanll@gmail.com>
2020-12-12 23:27:57 +0900

    fix: change database config

--- config/database.js
+++ config/database.js
@@ -1,21 +1,14 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
-
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
       options: {
-        ssl: false,
+        useNullAsDefault: true,
       },
     },
   },
